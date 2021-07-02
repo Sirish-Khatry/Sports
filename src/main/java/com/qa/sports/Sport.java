eb914b34398a65156d3c537083a03274eac38b40
DavidBarringerQA <DBarringer@qa.com>
2021-07-02 11:19:59 +0100

    Added sport abstract class

---
+++ src/main/java/com/qa/sports/Sport.java
@@ -0,0 +1,27 @@
+package com.qa.sport;
+
+public abstract class Sport{
+  private String name;
+  private int players;
+
+  public Sport(String name, int players){
+    this.name = name;
+    this.players = players;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getPlayers(){
+    return this.players;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setPlayers(int players){
+    this.players = players;
+  }
+}
